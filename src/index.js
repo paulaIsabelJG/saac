343b4a34fedaccd7cb2e00f8437053437b66a4cc
paulaIsabelJG <pijuanesgi.inf@upsa.es>
2025-05-28 23:57:16 +0200

    Serve Angular frontend correctly

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path'); // <--- Añade esto
 const app = express();
 const { config } = require('./config/index');
 const { logErrors, errorHandler } = require('./utils/middleware/errorHandlers');
@@ -13,7 +14,7 @@ const usuariosAPI = require('./routes/usuarios');
 console.log("✅ Backend iniciado");
 
 // Middlewares
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public/dist/programa-saac/browser'))); // <--- CAMBIA ESTA LÍNEA
 app.use(cors());
 app.use(express.json());
 
@@ -22,9 +23,9 @@ pictogramasAPI(app);
 frasesAPI(app);
 usuariosAPI(app);
 
-
-app.listen(config.port, () => {
-    console.log(`Servidor escuchando en puerto ${config.port}`);
+// "Catch-all" para el frontend Angular (SPA)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/dist/programa-saac/browser/index.html'));
 });
 
 // Middlewares de error (siempre después de las rutas)
@@ -32,3 +33,7 @@ app.use(logErrors);
 app.use(errorHandler);
 app.use(notFoundHandler);
 app.use(validationHandler);
+
+app.listen(config.port, () => {
+    console.log(`Servidor escuchando en puerto ${config.port}`);
+});
